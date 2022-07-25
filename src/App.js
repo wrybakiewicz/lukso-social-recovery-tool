7ae82a251e384c4e8c78f627621f8d52732d320f
Wojciech Rybakiewicz <wojtek.rybakiewicz@gmail.com>
2022-07-25 16:03:21 +0200

    interact with smart contract functions

--- src/App.js
+++ src/App.js
@@ -1,37 +1,84 @@
-import { ethers } from 'ethers';
-import {useEffect} from "react";
-import { ContractFactory } from 'ethers';
-import LSP11BasicSocialRecoveryInit from "@lukso/lsp-smart-contracts/artifacts/contracts/LSP11BasicSocialRecovery/LSP11BasicSocialRecoveryInit.sol/LSP11BasicSocialRecoveryInit.json";
+import {ContractFactory, ethers} from 'ethers';
+import {useEffect, useState} from "react";
+import LSP11BasicSocialRecoveryInit
+    from "@lukso/lsp-smart-contracts/artifacts/contracts/LSP11BasicSocialRecovery/LSP11BasicSocialRecoveryInit.sol/LSP11BasicSocialRecoveryInit.json";
 
 
 export default function App() {
 
-  const initialize = async () => {
-    console.log("Initializing")
-    const etherProvider = new ethers.providers.Web3Provider(window.ethereum);
+    const [signer, setSigner] = useState()
+    const [contract, setContract] = useState()
+    const [newGuardian, setNewGuardian] = useState()
+    const [isGuardian, setIsGuardian] = useState()
 
-    const accountsRequest = await etherProvider.send(
-        'eth_requestAccounts',
-        [],
-    );
-    const signer = etherProvider.getSigner();
-    const address = await signer.getAddress();
-    console.log(address)
+    const initialize = async () => {
+        console.log("Initializing")
+        const etherProvider = new ethers.providers.Web3Provider(window.ethereum);
 
-    const contractFactory = ContractFactory.fromSolidity(LSP11BasicSocialRecoveryInit, signer)
+        await etherProvider.send(
+            'eth_requestAccounts',
+            [],
+        );
+        const signer = etherProvider.getSigner();
+        const address = await signer.getAddress();
+        console.log(address)
 
-    const contract = await contractFactory.deploy()
+        setSigner(signer)
 
-    console.log(contract)
+        console.log("Initialized")
+    }
 
-    console.log("Initialized")
-  }
+    const deploySocialRecovery = async () => {
+        console.log("Deploying social recovery")
+        const contractFactory = ContractFactory.fromSolidity(LSP11BasicSocialRecoveryInit, signer)
+        const contract = await contractFactory.deploy()
 
-  useEffect(() => {
-    initialize()
-  }, [])
+        setContract(contract)
 
-  return <div>
-    <button>Deploy</button>
-  </div>
+        console.log(contract)
+    }
+
+    const addGuardian = async () => {
+        console.log("Adding guardian")
+
+        await contract.addGuardian(newGuardian)
+
+        console.log("Added guardian")
+    }
+
+    const updateIsGuardian = async (address) => {
+        console.log("Updating is guardian: " + address)
+        const threshold = await contract.getGuardiansThreshold()
+        console.log(threshold)
+        console.log("Updated is guardian")
+    }
+
+    useEffect(() => {
+        initialize()
+    }, [])
+
+    if (!signer) {
+        return <div>Loading...</div>
+    }
+
+    return <div>
+        <div>
+            <h3>My recovery</h3>
+            {contract ? null :
+                <div>
+                    <button onClick={deploySocialRecovery}>Deploy</button>
+                </div>
+            }
+            <div>
+                <h4>Add guardian</h4>
+                <input type={"text"} onChange={e => setNewGuardian(e.target.value)}/>
+                {newGuardian ? <button onClick={addGuardian}>Add guardian</button> : null}
+            </div>
+            <div>
+                <h4>Is guardian</h4>
+                <input type={"text"} onChange={e => updateIsGuardian(e.target.value)}/>
+                {isGuardian !== undefined ? isGuardian : null}
+            </div>
+        </div>
+    </div>
 }
