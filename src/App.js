6866650ca1b2fd696c54b6de8cdee7270abcdf09
Wojciech Rybakiewicz <wojtek.rybakiewicz@gmail.com>
2022-07-25 12:00:50 +0200

    social recovery deployment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { ethers } from 'ethers';
+import {useEffect} from "react";
+import { ContractFactory } from 'ethers';
+import LSP11BasicSocialRecoveryInit from "@lukso/lsp-smart-contracts/artifacts/contracts/LSP11BasicSocialRecovery/LSP11BasicSocialRecoveryInit.sol/LSP11BasicSocialRecoveryInit.json";
+
 
 export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const initialize = async () => {
+    console.log("Initializing")
+    const etherProvider = new ethers.providers.Web3Provider(window.ethereum);
+
+    const accountsRequest = await etherProvider.send(
+        'eth_requestAccounts',
+        [],
+    );
+    const signer = etherProvider.getSigner();
+    const address = await signer.getAddress();
+    console.log(address)
+
+    const contractFactory = ContractFactory.fromSolidity(LSP11BasicSocialRecoveryInit, signer)
+
+    const contract = await contractFactory.deploy()
+
+    console.log(contract)
+
+    console.log("Initialized")
+  }
+
+  useEffect(() => {
+    initialize()
+  }, [])
+
+  return <div>
+    <button>Deploy</button>
+  </div>
 }
