79faad7e990ccaabbc47d1750fb84ff85a7f8c3d
Wojciech Rybakiewicz <wojtek.rybakiewicz@gmail.com>
2022-07-27 11:46:52 +0200

    play with not proxy contract

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import {ContractFactory, ethers} from 'ethers';
 import {useEffect, useState} from "react";
-import LSP11BasicSocialRecoveryInit
-    from "@lukso/lsp-smart-contracts/artifacts/contracts/LSP11BasicSocialRecovery/LSP11BasicSocialRecoveryInit.sol/LSP11BasicSocialRecoveryInit.json";
+import LSP11BasicSocialRecovery
+    from "@lukso/lsp-smart-contracts/artifacts/contracts/LSP11BasicSocialRecovery/LSP11BasicSocialRecovery.sol/LSP11BasicSocialRecovery.json";
 
 
 export default function App() {
@@ -30,25 +30,29 @@ export default function App() {
 
     const deploySocialRecovery = async () => {
         console.log("Deploying social recovery")
-        const contractFactory = ContractFactory.fromSolidity(LSP11BasicSocialRecoveryInit, signer)
-        const contract = await contractFactory.deploy()
+        const contractFactory = ContractFactory.fromSolidity(LSP11BasicSocialRecovery, signer)
         const address = await signer.getAddress()
-        await contract.initialize(address)
-        await contract.setThreshold(1)
+        const contract = await contractFactory.deploy(address)
+        // const contract = await ContractFactory.getContract("0x093a5a8393fc11e23dba19fe41194b1cb78f4132", LSP11BasicSocialRecovery, signer)
+        // await contract.setThreshold(1)
 
         setContract(contract)
 
         console.log(contract)
 
-        const threshold = await contract.getGuardiansThreshold()
-        console.log(threshold)
+        // const threshold = await contract.getGuardiansThreshold()
+        // console.log(threshold)
     }
 
     const addGuardian = async () => {
         console.log("Adding guardian")
 
-        await contract.addGuardian(newGuardian)
+        // await contract.addGuardian(newGuardian)
+        const contract = await ContractFactory.getContract("0x093a5a8393fc11e23dba19fe41194b1cb78f4132", LSP11BasicSocialRecovery.abi, signer)
+        console.log(await contract.getGuardiansThreshold())
 
+        await contract.addGuardian("0x70EbbE40C41295bC757B50dE4325cb53a172F490")
+        await contract.setThreshold(1)
         console.log("Added guardian")
     }
 
