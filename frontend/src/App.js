97d4e6160396b9a84dca8723f28893e76aff0dcb
Wojciech Rybakiewicz <wojtek.rybakiewicz@gmail.com>
2022-07-28 21:27:14 +0200

    play with layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import {ContractFactory, ethers} from 'ethers';
-import {useEffect, useState} from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 import LSP11BasicSocialRecovery
     from "@lukso/lsp-smart-contracts/artifacts/contracts/LSP11BasicSocialRecovery/LSP11BasicSocialRecovery.sol/LSP11BasicSocialRecovery.json";
-
+import './App.css'
+import Button from 'react-bootstrap/Button';
 
 export default function App() {
 
@@ -26,6 +28,19 @@ export default function App() {
         setSigner(signer)
 
         console.log("Initialized")
+
+        const url = `https://f039pk1upb.execute-api.eu-central-1.amazonaws.com/api/getrecoverycontractaddressforaddress?address=0x70ebbe40c41295bc757b50de4325cb53a172f490`
+        await axios.get(url)
+            .then((response) => {
+                console.log(response.data)
+            })
+
+        // const url2 = `https://f039pk1upb.execute-api.eu-central-1.amazonaws.com/api/addrecoverycontractaddress`
+        // await axios.post(url2, {txHash: "0x50977eacc421d5f7b27b5dfc39854ffb8c12286ba49fb2e959b345b3c8f302fa"})
+        //     .then((response) => {
+        //         console.log(response.data)
+        //     })
+        //
     }
 
     const deploySocialRecovery = async () => {
@@ -67,28 +82,42 @@ export default function App() {
         initialize()
     }, [])
 
-    if (!signer) {
-        return <div>Loading...</div>
-    }
+    // if (!signer) {
+    //     return <div>Loading...</div>
+    // }
 
     return <div>
-        <div>
-            <h3>My recovery</h3>
-            {contract ? null :
-                <div>
-                    <button onClick={deploySocialRecovery}>Deploy</button>
-                </div>
-            }
-            <div>
-                <h4>Add guardian</h4>
-                <input type={"text"} onChange={e => setNewGuardian(e.target.value)}/>
-                {newGuardian ? <button onClick={addGuardian}>Add guardian</button> : null}
-            </div>
-            <div>
-                <h4>Is guardian</h4>
-                <input type={"text"} onChange={e => updateIsGuardian(e.target.value)}/>
-                {isGuardian !== undefined ? isGuardian : null}
-            </div>
+        <div className={"centered-header"}>LUKSO Social recovery</div>
+        <div className={"view-recovery"}>
+            <input placeholder={"Address"} type={"text"}/>
+
+            <Button variant="primary">View recovery</Button>{' '}
+
+        </div>
+        <div className={"left centered-button"}>
+            <Button variant="outline-primary btn-lg">Your Social Recovery</Button>{' '}
+        </div>
+        <div className={"right centered-button"}>
+            <Button variant="primary btn-lg">You as a Guardian</Button>{' '}
         </div>
     </div>
+    // <div>
+    {/*<div>*/
+    }
+    {/*    <h3>My recovery</h3>*/
+    }
+    {/*    {contract ? null :*/
+    }
+    {/*        <div>*/
+    }
+    {/*            <button onClick={deploySocialRecovery}>Deploy</button>*/
+    }
+    {/*        </div>*/
+    }
+    {/*    }}*/
+    }
+    {/*</div>*/
+    }
+    {/*</div>*/
+    }
 }
