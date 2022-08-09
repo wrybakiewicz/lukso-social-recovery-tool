d389c0ebecfe31c0f325c401add9cf3c15934561
Wojciech Rybakiewicz <wojtek.rybakiewicz@gmail.com>
2022-08-09 15:02:04 +0200

    add & remove guardians with threshold

--- contract/contracts/SocialRecovery.sol
+++ contract/contracts/SocialRecovery.sol
@@ -6,15 +6,34 @@ import {OwnableUnset} from "@erc725/smart-contracts/contracts/custom/OwnableUnse
 
 
 contract SocialRecovery is LSP11BasicSocialRecoveryCore {
+    using EnumerableSet for EnumerableSet.AddressSet;
 
     constructor(address _account, bytes32 _initialSecretHash) {
         account = _account;
         OwnableUnset._setOwner(_account);
         _secretHash = _initialSecretHash;
+        _guardiansThreshold = 1;
+    }
+
+    function addGuardianWithThresholdUpdate(address newGuardian) public onlyOwner {
+        addGuardian(newGuardian);
+        updateThreshold();
+    }
+
+    function removeGuardianWithThresholdUpdate(address guardian) public onlyOwner {
+        require(_guardians.contains(guardian), "Provided address is not a guardian");
+        _guardians.remove(guardian);
+        updateThreshold();
     }
 
     function getSecretHash() public view returns (bytes32) {
         return _secretHash;
+    }
+
+    function updateThreshold() internal {
+        uint256 threshold = _guardians.length();
+        uint256 newThreshold = threshold % 2 == 0 ? threshold / 2 : (threshold + 1) / 2;
+        _guardiansThreshold = newThreshold > 0 ? newThreshold : 1;
     }
 
 }
--- contract/test/socialRecovery.test.js
+++ contract/test/socialRecovery.test.js
@@ -5,29 +5,176 @@ const {toUtf8Bytes} = require("@ethersproject/strings");
 
 describe("Social Recovery", function () {
 
-    it("should deploy contract", async function () {
-        const [owner] = await ethers.getSigners();
-        const secret1 = "123"
-        const secretHash1 = keccak256(toUtf8Bytes(secret1));
+    const secret1 = "123"
+    const secretHash1 = keccak256(toUtf8Bytes(secret1));
+
+    const deployContract = async (account) => {
         const SocialRecovery = await ethers.getContractFactory("SocialRecovery");
-        const socialRecovery = await SocialRecovery.deploy(owner.address, secretHash1);
+        return await SocialRecovery.deploy(account.address, secretHash1);
+    }
+
+    it("should deploy contract", async () => {
+        const [owner] = await ethers.getSigners();
+        const socialRecovery = await deployContract(owner)
 
         expect(await socialRecovery.account()).to.equal(owner.address)
         expect(await socialRecovery.getSecretHash()).to.equal(secretHash1)
+        expect(await socialRecovery.getGuardiansThreshold()).to.equal(1)
     });
 
-    it("should set and return secret hash contract", async function () {
+    it("should set and return secret hash contract", async () => {
         const [owner] = await ethers.getSigners();
-        const secret1 = "123"
-        const secretHash1 = keccak256(toUtf8Bytes(secret1));
-        const SocialRecovery = await ethers.getContractFactory("SocialRecovery");
-        const socialRecovery = await SocialRecovery.deploy(owner.address, secretHash1);
+        const socialRecovery = await deployContract(owner)
         const secret2 = "abc"
         const secretHash2 = keccak256(toUtf8Bytes(secret2));
 
         await socialRecovery.setSecret(secretHash2)
 
         expect(await socialRecovery.getSecretHash()).to.equal(secretHash2)
+    });
+
+    it("should add one guardian with threshold update", async () => {
+        const [owner, address1] = await ethers.getSigners();
+        const socialRecovery = await deployContract(owner)
+
+        await socialRecovery.addGuardianWithThresholdUpdate(address1.address)
+
+        const guardians = await socialRecovery.getGuardians()
+        expect(guardians.length).to.equal(1)
+        expect(guardians[0]).to.equal(address1.address)
+        expect(await socialRecovery.getGuardiansThreshold()).to.equal(1)
+    });
+
+    it("should add two guardians with threshold update", async () => {
+        const [owner, address1, address2] = await ethers.getSigners();
+        const socialRecovery = await deployContract(owner)
+
+        await socialRecovery.addGuardianWithThresholdUpdate(address1.address)
+        await socialRecovery.addGuardianWithThresholdUpdate(address2.address)
+
+        const guardians = await socialRecovery.getGuardians()
+        expect(guardians.length).to.equal(2)
+        expect(guardians[0]).to.equal(address1.address)
+        expect(await socialRecovery.getGuardiansThreshold()).to.equal(1)
+    });
+
+    it("should add three guardians with threshold update", async () => {
+        const [owner, address1, address2, address3] = await ethers.getSigners();
+        const socialRecovery = await deployContract(owner)
+
+        await socialRecovery.addGuardianWithThresholdUpdate(address1.address)
+        await socialRecovery.addGuardianWithThresholdUpdate(address2.address)
+        await socialRecovery.addGuardianWithThresholdUpdate(address3.address)
+
+        const guardians = await socialRecovery.getGuardians()
+        expect(guardians.length).to.equal(3)
+        expect(guardians[0]).to.equal(address1.address)
+        expect(await socialRecovery.getGuardiansThreshold()).to.equal(2)
+    });
+
+    it("should add four guardians with threshold update", async () => {
+        const [owner, address1, address2, address3, address4] = await ethers.getSigners();
+        const socialRecovery = await deployContract(owner)
+
+        await socialRecovery.addGuardianWithThresholdUpdate(address1.address)
+        await socialRecovery.addGuardianWithThresholdUpdate(address2.address)
+        await socialRecovery.addGuardianWithThresholdUpdate(address3.address)
+        await socialRecovery.addGuardianWithThresholdUpdate(address4.address)
+
+        const guardians = await socialRecovery.getGuardians()
+        expect(guardians.length).to.equal(4)
+        expect(guardians[0]).to.equal(address1.address)
+        expect(await socialRecovery.getGuardiansThreshold()).to.equal(2)
+    });
+
+    it("should add five guardians with threshold update", async () => {
+        const [owner, address1, address2, address3, address4, address5] = await ethers.getSigners();
+        const socialRecovery = await deployContract(owner)
+
+        await socialRecovery.addGuardianWithThresholdUpdate(address1.address)
+        await socialRecovery.addGuardianWithThresholdUpdate(address2.address)
+        await socialRecovery.addGuardianWithThresholdUpdate(address3.address)
+        await socialRecovery.addGuardianWithThresholdUpdate(address4.address)
+        await socialRecovery.addGuardianWithThresholdUpdate(address5.address)
+
+        const guardians = await socialRecovery.getGuardians()
+        expect(guardians.length).to.equal(5)
+        expect(guardians[0]).to.equal(address1.address)
+        expect(await socialRecovery.getGuardiansThreshold()).to.equal(3)
+    });
+
+    it("should remove guardian from five guardians with threshold update", async () => {
+        const [owner, address1, address2, address3, address4, address5] = await ethers.getSigners();
+        const socialRecovery = await deployContract(owner)
+        await socialRecovery.addGuardianWithThresholdUpdate(address1.address)
+        await socialRecovery.addGuardianWithThresholdUpdate(address2.address)
+        await socialRecovery.addGuardianWithThresholdUpdate(address3.address)
+        await socialRecovery.addGuardianWithThresholdUpdate(address4.address)
+        await socialRecovery.addGuardianWithThresholdUpdate(address5.address)
+
+        await socialRecovery.removeGuardianWithThresholdUpdate(address5.address)
+
+        const guardians = await socialRecovery.getGuardians()
+        expect(guardians.length).to.equal(4)
+        expect(guardians[0]).to.equal(address1.address)
+        expect(await socialRecovery.getGuardiansThreshold()).to.equal(2)
+    });
+
+    it("should remove guardian from four guardians with threshold update", async () => {
+        const [owner, address1, address2, address3, address4] = await ethers.getSigners();
+        const socialRecovery = await deployContract(owner)
+        await socialRecovery.addGuardianWithThresholdUpdate(address1.address)
+        await socialRecovery.addGuardianWithThresholdUpdate(address2.address)
+        await socialRecovery.addGuardianWithThresholdUpdate(address3.address)
+        await socialRecovery.addGuardianWithThresholdUpdate(address4.address)
+
+        await socialRecovery.removeGuardianWithThresholdUpdate(address4.address)
+
+        const guardians = await socialRecovery.getGuardians()
+        expect(guardians.length).to.equal(3)
+        expect(guardians[0]).to.equal(address1.address)
+        expect(await socialRecovery.getGuardiansThreshold()).to.equal(2)
+    });
+
+    it("should remove guardian from three guardians with threshold update", async () => {
+        const [owner, address1, address2, address3] = await ethers.getSigners();
+        const socialRecovery = await deployContract(owner)
+        await socialRecovery.addGuardianWithThresholdUpdate(address1.address)
+        await socialRecovery.addGuardianWithThresholdUpdate(address2.address)
+        await socialRecovery.addGuardianWithThresholdUpdate(address3.address)
+
+        await socialRecovery.removeGuardianWithThresholdUpdate(address3.address)
+
+        const guardians = await socialRecovery.getGuardians()
+        expect(guardians.length).to.equal(2)
+        expect(guardians[0]).to.equal(address1.address)
+        expect(await socialRecovery.getGuardiansThreshold()).to.equal(1)
+    });
+
+    it("should remove guardian from two guardians with threshold update", async () => {
+        const [owner, address1, address2] = await ethers.getSigners();
+        const socialRecovery = await deployContract(owner)
+        await socialRecovery.addGuardianWithThresholdUpdate(address1.address)
+        await socialRecovery.addGuardianWithThresholdUpdate(address2.address)
+
+        await socialRecovery.removeGuardianWithThresholdUpdate(address2.address)
+
+        const guardians = await socialRecovery.getGuardians()
+        expect(guardians.length).to.equal(1)
+        expect(guardians[0]).to.equal(address1.address)
+        expect(await socialRecovery.getGuardiansThreshold()).to.equal(1)
+    });
+
+    it("should remove guardian from one guardian with threshold update", async () => {
+        const [owner, address1] = await ethers.getSigners();
+        const socialRecovery = await deployContract(owner)
+        await socialRecovery.addGuardianWithThresholdUpdate(address1.address)
+
+        await socialRecovery.removeGuardianWithThresholdUpdate(address1.address)
+
+        const guardians = await socialRecovery.getGuardians()
+        expect(guardians.length).to.equal(0)
+        expect(await socialRecovery.getGuardiansThreshold()).to.equal(1)
     });
 
 })
