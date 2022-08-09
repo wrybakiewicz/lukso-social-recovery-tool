b9b9dae3d6f0ad0f995633c811cd9163b00e006a
Wojciech Rybakiewicz <wojtek.rybakiewicz@gmail.com>
2022-08-09 15:15:22 +0200

    recover without secret

--- contract/contracts/SocialRecovery.sol
+++ contract/contracts/SocialRecovery.sol
@@ -26,14 +26,64 @@ contract SocialRecovery is LSP11BasicSocialRecoveryCore {
         updateThreshold();
     }
 
+    function recoverOwnershipWithoutSecret(
+        bytes32 recoverProcessId,
+        bytes32 newHash
+    ) public NotZeroBytes32(newHash) {
+        _checkRequirements(recoverProcessId);
+
+        _secretHash = newHash;
+        // Starting new recovery counter
+    unchecked {
+        _recoveryCounter++;
+    }
+
+        address keyManager = ERC725(account).owner();
+        require(
+            ERC165Checker.supportsERC165Interface(keyManager, _INTERFACEID_LSP6),
+            "Owner of account doesn't support LSP6 InterfaceId"
+        );
+
+        // Setting permissions for `msg.sender`
+        (bytes32[] memory keys, bytes[] memory values) = LSP6Utils
+        .createPermissionsKeysForController(
+            ERC725(account),
+            msg.sender,
+            abi.encodePacked(_ALL_DEFAULT_PERMISSIONS)
+        );
+
+        LSP6Utils.setDataViaKeyManager(keyManager, keys, values);
+    }
+
     function getSecretHash() public view returns (bytes32) {
         return _secretHash;
     }
 
     function updateThreshold() internal {
-        uint256 threshold = _guardians.length();
-        uint256 newThreshold = threshold % 2 == 0 ? threshold / 2 : (threshold + 1) / 2;
+        uint256 guardiansCount = _guardians.length();
+        uint256 newThreshold = guardiansCount % 2 == 0 ? guardiansCount / 2 : (guardiansCount + 1) / 2;
         _guardiansThreshold = newThreshold > 0 ? newThreshold : 1;
+    }
+
+    function _checkRequirements(bytes32 recoverProcessId) internal view {
+        uint256 guardiansCount = _guardians.length();
+        require(guardiansCount >= 2, "Minimum guardians count to recover without secret is 2");
+
+        uint256 recoverCounter = _recoveryCounter;
+        uint256 senderVotes;
+        uint256 guardiansLength = _guardians.length();
+
+    unchecked {
+        for (uint256 i = 0; i < guardiansLength; i++) {
+            if (
+                _guardiansVotes[recoverCounter][recoverProcessId][_guardians.at(i)] ==
+                msg.sender
+            ) {
+                senderVotes++;
+            }
+        }
+    }
+        require(senderVotes == _guardians.length(), "All guardians needs to vote for you");
     }
 
 }
